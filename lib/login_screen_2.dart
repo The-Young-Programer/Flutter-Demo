ac250b815f854083691cc990955926e7af4af83d
The-Young-Programer <abduramonibrahim@gmail.com>
2024-12-06 19:46:04 +0100

    First commit

---
+++ lib/login_screen_2.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen2 extends StatelessWidget {
+  final Color? backgroundColor1;
+  final Color? backgroundColor2;
+  final Color? highlightColor;
+  final Color? foregroundColor;
+  final AssetImage? logo;
+
+  LoginScreen2({
+    Key? k,
+    this.backgroundColor1 = const Color(0xFF444152),
+    this.backgroundColor2 = const Color(0xFF6f6c7d),
+    this.highlightColor = const Color(0xfff65aa3),
+    this.foregroundColor = Colors.white,
+    this.logo = const AssetImage("assets/images/full-bloom.png"),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment(1.0, 0.0),
+              colors: [this.backgroundColor1!, this.backgroundColor2!],
+              tileMode: TileMode.repeated,
+            ),
+          ),
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.only(top: 110.0, bottom: 50.0),
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 128.0,
+                        width: 128.0,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          foregroundColor: this.foregroundColor,
+                          radius: 100.0,
+                          child: Text(
+                            "S",
+                            style: TextStyle(
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: this.foregroundColor!,
+                            width: 1.0,
+                          ),
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          "Samarth Agarwal",
+                          style: TextStyle(color: this.foregroundColor),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                        color: this.foregroundColor!,
+                        width: 0.5,
+                        style: BorderStyle.solid),
+                  ),
+                ),
+                padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: 10.0, bottom: 10.0, right: 00.0),
+                      child: Icon(
+                        Icons.alternate_email,
+                        color: this.foregroundColor,
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'samarthagarwal@live.com',
+                          hintStyle: TextStyle(color: this.foregroundColor),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin:
+                    const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                        color: this.foregroundColor!,
+                        width: 0.5,
+                        style: BorderStyle.solid),
+                  ),
+                ),
+                padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: 10.0, bottom: 10.0, right: 00.0),
+                      child: Icon(
+                        Icons.lock_open,
+                        color: this.foregroundColor,
+                      ),
+                    ),
+                    Expanded(
+                      child: TextField(
+                        obscureText: true,
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '*********',
+                          hintStyle: TextStyle(color: this.foregroundColor),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin:
+                    const EdgeInsets.only(left: 40.0, right: 40.0, top: 30.0),
+                alignment: Alignment.center,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 20.0, horizontal: 20.0),
+                          backgroundColor: this.highlightColor,
+                        ),
+                        onPressed: () => {},
+                        child: Text(
+                          "Log In",
+                          style: TextStyle(color: this.foregroundColor),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin:
+                    const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
+                alignment: Alignment.center,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 20.0, horizontal: 20.0),
+                          backgroundColor: Colors.transparent,
+                        ),
+                        onPressed: () => {},
+                        child: Text(
+                          "Forgot your password?",
+                          style: TextStyle(
+                              color: this.foregroundColor!.withOpacity(0.5)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Divider(),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(
+                    left: 40.0, right: 40.0, top: 10.0, bottom: 20.0),
+                alignment: Alignment.center,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 20.0, horizontal: 20.0),
+                          backgroundColor: Colors.transparent,
+                        ),
+                        onPressed: () => {},
+                        child: Text(
+                          "Don't have an account? Create One",
+                          style: TextStyle(
+                              color: this.foregroundColor!.withOpacity(0.5)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
